c823c91c67cde71553ee9dd7110ae8fe439aacf2
oceanp8 <148895610+oceanp8@users.noreply.github.com>
2024-09-11 02:29:08 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String ocean = "Ocean";
+        int two = 2;
+        two = two + 2;
+        double four = 4.4;
+        four = four - 4;
+        boolean b = true;
+        final int ten = 10;
+
+        System.out.println(ocean + two + four + b + ten);
+        System.out.println(two+four);
+
+    }
+}
